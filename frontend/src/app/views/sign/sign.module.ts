136e5dddd842b8ba22f54047c30046b017661b8e
bartoszborys <borysbartosz1@gmail.com>
2019-08-08 21:23:50 +0200

    update sign info view

--- frontend/src/app/views/sign/sign.module.ts
+++ frontend/src/app/views/sign/sign.module.ts
@@ -5,12 +5,16 @@ import { SignRoutingModule } from './sign-routing.module';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { SignInfoComponent } from './components/sign-info/sign-info.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [SignComponent, SignInComponent, SignUpComponent, SignInfoComponent],
   imports: [
     CommonModule,
-    SignRoutingModule
+    SignRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule
   ]
 })
 export class SignModule { }
